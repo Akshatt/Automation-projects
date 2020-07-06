0c486a42f8994a9f419948e849d21414fcbfdee5
Akshat Jain <jain.akshat31@gmail.com>
2020-07-06 16:56:48 +0530

    Showing private and public repos separately

--- automate.py
+++ automate.py
@@ -4,14 +4,13 @@ import stdiomask
 # Github username and password
 username = input("Enter GitHub username: ")
 print("To include private repos as well, GitHub needs to be authenticated.\n\
-        This will require you to enter your password.")
-print("If you don't wish to do so, simply press Enter when asked for password")
+        This will require you to enter the password for {}.\n\
+If you don't wish to do so, simply press Enter when asked for password".format(username))
 password = stdiomask.getpass(prompt='Enter Password: ')
 
 # pygithub object
 if password == "":
-    g = Github()
-    user = g.get_user(username)
+    user = Github().get_user(username)
     private = 0
 else:
     g = Github(username, password)
@@ -19,16 +18,31 @@ else:
     private = 1
 
 
-def findreadme(repo):
+def assignrepos(repo):
     try:
         if repo.get_contents("README.md"):
             return False
     except:
+        if private == 1 and repo.private:
+            private_repos.append(repo)
+        else:
+            public_repos.append(repo)
         return True
 
 
-no_readme_repos = []
+private_repos = []
+public_repos = []
 for repo in user.get_repos():
-    if findreadme(repo):
-        no_readme_repos.append(repo)
+    assignrepos(repo)
+
+
+def repoprint(status, repos):
+    print("\nFollowing {} repos don't have a README.md file:".format(status))
+    for repo in repos:
         print(repo)
+    return print()
+
+
+repoprint("public", public_repos)
+if private == 1:
+    repoprint("private", private_repos)
